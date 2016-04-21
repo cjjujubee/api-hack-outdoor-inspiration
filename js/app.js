e4be6fe80fc3e080f55389884499231a211d8206
cjjujubee <connie.jew@gmail.com>
2016-04-21 09:51:25 -0700

    Creates and structures  basic HTML layout for app

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/* allow user to input location (e.g. "New York City", "California", "Iceland", "10013") & store it somewhere 
+- use FlickrAPI to fetch the data in a json-p object 
+- extract the image data from the object- append to HTML 
+- sort images by interestingness
+- figure out how to do it again
+- also add photographer's name, the date the image was taken, and a link to the Flickr page- style it 
+- add buttons to sort by X attribute */
+
